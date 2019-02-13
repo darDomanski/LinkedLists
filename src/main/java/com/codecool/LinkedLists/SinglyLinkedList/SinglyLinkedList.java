d91244b9e83e327f665f29eabab894c7e227edf7
darDomanski <dardomanski@gmail.com>
2019-02-13 18:12:52 +0100

    Implemented SinglyLinkedList with all methods

--- src/main/java/com/codecool/LinkedLists/SinglyLinkedList/SinglyLinkedList.java
+++ src/main/java/com/codecool/LinkedLists/SinglyLinkedList/SinglyLinkedList.java
@@ -2,7 +2,6 @@ package com.codecool.LinkedLists.SinglyLinkedList;
 
 public class SinglyLinkedList<T> {
     private Node<T> head;
-    private Node<T> last;
     private int length = 0;
 
     public void add(T data) {
@@ -11,16 +10,81 @@ public class SinglyLinkedList<T> {
 
         if (head == null) {
             head = newNode;
-            last = head;
 
         } else {
             while (current.getNext() != null) {
                 current = current.getNext();
             }
-            current.getNext().setNext(newNode);
+            current.setNext(newNode);
         }
         length++;
     }
 
+    public T get(int index) {
+        if (index < 0 || index >= length) throw new IllegalArgumentException("Wrong index given!");
 
+        Node<T> current = head;
+        int counter = -1;
+
+        while (counter != index) {
+            counter++;
+
+            if (counter == index) {
+                break;
+            } else {
+                current = current.getNext();
+            }
+        }
+        return current.getData();
+    }
+
+    public void insert(int index, T data) {
+        if (index < 0 || index > length) throw new IllegalArgumentException("Wrong index given!");
+
+        Node<T> previous = null;
+        Node<T> current = head;
+        Node<T> newNode = new Node<T>(data);
+        int counter = 0;
+
+        if (index == 0) {
+
+            newNode.setNext(current);
+            head = newNode;
+
+        } else {
+
+            while (counter != index) {
+                previous = current;
+                current = current.getNext();
+                counter++;
+            }
+
+            previous.setNext(newNode);
+            newNode.setNext(current);
+        }
+
+        length++;
+    }
+
+    public void remove(T data) {
+        Node<T> current = head;
+
+        if (current != null) {
+
+            if (current.getData().equals(data)) {
+                head = current.getNext();
+
+            } else {
+                while (current.getNext() != null) {
+
+                    if (current.getNext().getData().equals(data)) {
+                        current.setNext(current.getNext().getNext());
+                    } else {
+                        current = current.getNext();
+                    }
+                }
+            }
+            length--;
+        }
+    }
 }
--- src/test/java/com/codecool/LinkedLists/DoublyLinkedListTest.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.LinkedLists;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class DoublyLinkedListTest {
-
-}
